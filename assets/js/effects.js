5f6ecab84db75a78a485fc516e18b0fb8539f3bd
JoshuaRobertson <josh.robertson@coinclear.uk>
2020-11-02 13:39:54 +0000

    Added a change function to loop over pixel data and increment by 4 for RGBA values

--- assets/js/effects.js
+++ assets/js/effects.js
@@ -1,13 +1,39 @@
+// Select canvas & context
 const canvas = document.getElementById('canvas')
 const ctx    = canvas.getContext('2d')
 
-const img = new Image()
-const imgPath = 'assets/img/'
+// Create new image based on the image uploaded
+const img         = new Image()
+const reader      = new FileReader()
+const imageLoader = document.getElementById('uploader')
 
-// Drawing to the canvas with a default image
-img.src = `${imgPath}bridge.jpg`
-img.onload = () => {
-  canvas.width = img.width
-  canvas.height = img.height
-  ctx.drawImage(img, 0, 0)
+// Upload image and draw to the canvas
+function uploadImage (e) {
+  reader.readAsDataURL(e.target.files[0])
+  reader.onload = () => {
+    img.src = reader.result
+    img.onload = () => {
+      canvas.width = img.width
+      canvas.height = img.height
+      ctx.drawImage(img, 0, 0)
+    }
+  }
 }
+
+function change () {
+  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+  const data = imageData.data
+
+  // Loop over pixel data and increment by 4 (RGBA)
+  for (let i = 0; i < data.length; i += 4) {
+    data[i]
+    data[i + 1]
+    data[i + 2]
+  }
+
+  ctx.putImageData(imageData, 0, 0)
+}
+
+document.querySelectorAll('button')[0].addEventListener('click', change)
+
+imageLoader.addEventListener('change', uploadImage)
